18f031bc4adec1112ed53889a5ff236066278d07
ljm <1914660874@qq.com>
2020-04-12 20:55:21 +0800

    新建User提交到本地仓库

---
+++ interview/src/main/java/com/ljm/User.java
@@ -0,0 +1,17 @@
+package com.ljm;/**
+ * @Author LJM
+ * @Date 2020/4/12 20:53
+ * @Version 1.0
+ */
+
+/**
+ * @ClassName User
+ * @Description 测试git
+ * @Author ljm
+ * @Date 20:53 2020/4/12
+ * @Version 2.1
+ **/
+public class User {
+    private String name;
+    private Integer id;
+}
